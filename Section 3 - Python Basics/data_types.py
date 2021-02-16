60888f73e4c017f173a02b84570653d423105ebf
connorworthen <connorw220@protonmail.com>
2021-02-15 19:54:43 -0800

    mathn functions

--- Section 3 - Python Basics/data_types.py
+++ Section 3 - Python Basics/data_types.py
@@ -1,9 +1,19 @@
 # Fundamental Data Types
-int
-float
-bool
-str
-list
-tuple
-set
-dict
+# int & float
+
+# Math Functions
+print(round(3.1))  # rounds return 3
+print(abs(-20))  # no negatives return 20
+
+# bool
+# str
+# list
+# tuple
+# set
+# dict
+
+# Classes -> custom types
+
+# Specialized Data Types
+
+# None = 0 / null
